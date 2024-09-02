0eb899e347de6d19a00e24903a3eaf55faff0dc0
CKNKP <kesarinandankumar2001@gmail.com>
2024-09-02 16:27:45 +0530

    Submit Request

--- src/components/Admin/Request/Request.jsx
+++ src/components/Admin/Request/Request.jsx
@@ -365,7 +365,14 @@ function Request() {
                       </div>
                     </div>
                   </div>
-
+                  <div className="flex items-center justify-end p-4">
+                  <button
+                    type="submit"
+                    className="px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-150 ease-in-out"
+                  >
+                    Submit
+                  </button>
+                </div>
               </div>
               
             </div>
